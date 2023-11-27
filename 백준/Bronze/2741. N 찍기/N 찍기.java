a6e5ad64c8d261d69ff9d9d694f8db96d061a5ed
miyeon lee <leeyun92@naver.com>
2023-11-27 21:35:27 +0900

    [Bronze V] Title: N 찍기, Time: 304 ms, Memory: 20852 KB -BaekjoonHub

--- 백준/Bronze/2741. N 찍기/N 찍기.java
+++ 백준/Bronze/2741. N 찍기/N 찍기.java
@@ -1,12 +1,18 @@
-import java.util.Scanner;
+import java.io.*;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int n = Integer.parseInt(br.readLine());
+        br.close();
+
 
         for (int i = 1; i <= n; i++) {
-            System.out.println(i);
+            bw.write(i + "\n");
         }
+        bw.flush();
+        bw.close();
     }
 }
