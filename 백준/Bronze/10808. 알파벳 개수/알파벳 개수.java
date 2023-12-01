ab34d32766a52f104157e07a1d302c4cbc15ce78
miyeon lee <leeyun92@naver.com>
2023-12-01 15:16:21 +0900

    [Bronze IV] Title: 알파벳 개수, Time: 140 ms, Memory: 16160 KB -BaekjoonHub

--- 백준/Bronze/10808. 알파벳 개수/알파벳 개수.java
+++ 백준/Bronze/10808. 알파벳 개수/알파벳 개수.java
@@ -4,12 +4,10 @@ public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         String input = br.readLine();
-
-        int[] count = new int[('z' - 'a') + 1];
-        int index = 0;
+        
+        int[] count = new int[26];
         for (int i = 0; i < input.length(); i++) {
-           index = input.charAt(i) - 97;
-           count[index]++;
+           count[input.charAt(i) - 97]++;
         }
 
         StringBuilder sb = new StringBuilder();
