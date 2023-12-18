45db1e031d846c6b5e027c4d2fb3c6682e627436
Miyeon Lee <leeyun92@naver.com>
2023-12-18 23:59:01 +0900

    [Silver V] Title: 방 번호, Time: 128 ms, Memory: 14200 KB -BaekjoonHub

--- 백준/Silver/1475. 방 번호/방 번호.java
+++ 백준/Silver/1475. 방 번호/방 번호.java
@@ -3,25 +3,16 @@ import java.io.*;
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        String n = br.readLine();
-        br.close();
-
-        int[] counts = new int[9];
-        int num = 0;
-        int result = 1;
-        for (int i = 0; i < n.length(); i++) {
-            num = n.charAt(i) - '0';
-            if (num == 6 || num == 9) {
-                num = 6;
-                if (counts[num] == (2 * result)) {
-                    result++;
-                }
-                counts[num]++;
-            } else {
-                if (counts[num] == result) {
-                    result++;
-                }
-                counts[num]++;
+        String N = br.readLine();
+        int[] counts = new int[10];
+        for (int i = 0; i < N.length(); i++) {
+            counts[N.charAt(i) - '0']++;
+        }
+        counts[6] = (int)Math.round((counts[6] + counts[9]) / (double)2);
+        int result = 0;
+        for (int i = 0; i < 9; i++) {
+            if (result < counts[i]) {
+                result = counts[i];
             }
         }
         System.out.println(result);
