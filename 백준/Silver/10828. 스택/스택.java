9b42578114593d9bfe7a77862a393ca481890677
Miyeon Lee <leeyun92@naver.com>
2024-01-06 11:56:33 +0900

    [Silver IV] Title: 스택, Time: 212 ms, Memory: 19376 KB -BaekjoonHub

--- 백준/Silver/10828. 스택/스택.java
+++ 백준/Silver/10828. 스택/스택.java
@@ -6,56 +6,29 @@ public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        MyStack myStack = new MyStack();
+
+        int[] stack = new int[10000];
+        int topIndex = -1;
 
         int N = Integer.parseInt(br.readLine());
+
         while (N-- > 0) {
             StringTokenizer st = new StringTokenizer(br.readLine());
             String cmd = st.nextToken();
             if (cmd.equals("push")) {
-                myStack.push(Integer.parseInt(st.nextToken()));
+                stack[++topIndex] = Integer.parseInt(st.nextToken());
             } else if (cmd.equals("pop")) {
-                bw.write(myStack.isEmpty() ? "-1\n" : myStack.pop() + "\n");
+                bw.write(topIndex == -1 ? "-1\n" : stack[topIndex--] + "\n");
             } else if (cmd.equals("size")) {
-                bw.write(myStack.size() + "\n");
+                bw.write(topIndex + 1 + "\n");
             } else if (cmd.equals("empty")) {
-                bw.write(myStack.isEmpty() ? "1\n" : "0\n");
+                bw.write(topIndex == -1 ? "1\n" : "0\n");
             } else if (cmd.equals("top")) {
-                bw.write(myStack.isEmpty() ? "-1\n" : myStack.top() + "\n");
+                bw.write(topIndex == -1 ? "-1\n" : stack[topIndex] + "\n");
             }
         }
         br.close();
         bw.flush();
         bw.close();
-    }
-}
-
-class MyStack {
-    private int[] stack = new int[10000];
-    private int topIndex = -1;
-    private int size = 0;
-
-    public void push(int value) {
-        stack[++topIndex] = value;
-        size++;
-    }
-
-    public int pop() {
-        int value = stack[topIndex];
-        stack[topIndex--] = 0;
-        size--;
-        return value;
-    }
-
-    public int size() {
-        return size;
-    }
-
-    public boolean isEmpty() {
-        return size == 0;
-    }
-
-    public int top() {
-        return stack[topIndex];
     }
 }
