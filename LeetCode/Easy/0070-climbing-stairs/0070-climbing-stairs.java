048bcf3c6a8f79e6e29295ef9252b350ad4f0b45
Miyeon Lee <leeyun92@naver.com>
2024-02-16 22:56:34 +0900

    [Easy] Time: 0 ms (100.00%), Memory: 40.8 MB (8.10%) - LeetHub

--- LeetCode/Easy/0070-climbing-stairs/0070-climbing-stairs.java
+++ LeetCode/Easy/0070-climbing-stairs/0070-climbing-stairs.java
@@ -1,21 +1,13 @@
-// Top-Down
+// Bottom-up
 class Solution {
-    int[] memo;
-
     public int climbStairs(int n) {
-        memo = new int[n + 1];
-        Arrays.fill(memo, -1);
-        return dp(n);
-    }
+        int[] dp = new int[n + 1];
+        dp[0] = 1;
+        dp[1] = 1;
 
-    public int dp(int n) {
-        if (n == 0 || n == 1) {
-            return 1;
+        for (int i = 2; i < n + 1; i++) {
+            dp[i] = dp[i - 1] + dp[i - 2];
         }
-        else if (memo[n] == -1) {
-            memo[n] = dp(n - 1) + dp(n - 2);
-        }
-        return memo[n];
-
+        return dp[n];
     }
 }
