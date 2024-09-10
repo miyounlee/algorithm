7359e5d7cb02651f0d30f079c57d43dc060dfba1
Miyeon Lee <leeyun92@naver.com>
2024-09-10 10:58:32 +0900

    [Bronze V] Title: 킹, 퀸, 룩, 비숍, 나이트, 폰, Time: 100 ms, Memory: 14260 KB -BaekjoonHub

--- 백준/Bronze/3003. 킹， 퀸， 룩， 비숍， 나이트， 폰/킹， 퀸， 룩， 비숍， 나이트， 폰.java
+++ 백준/Bronze/3003. 킹， 퀸， 룩， 비숍， 나이트， 폰/킹， 퀸， 룩， 비숍， 나이트， 폰.java
@@ -4,20 +4,15 @@ import java.util.StringTokenizer;
 public class Main {
 
     public static void main(String[] args) throws IOException {
-        int[] standard = new int[6];
-        standard[0] = 1;
-        standard[1] = 1;
-        standard[2] = 2;
-        standard[3] = 2;
-        standard[4] = 2;
-        standard[5] = 8;
-
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
         StringBuilder sb = new StringBuilder();
 
+        int[] chess = {1, 1, 2, 2, 2, 8};
+
         for (int i = 0; i < 6; i++) {
-            sb.append(standard[i] - Integer.parseInt(st.nextToken()) + " ");
+            int b = Integer.parseInt(st.nextToken());
+            sb.append(chess[i] - b).append(" ");
         }
 
         System.out.println(sb);
