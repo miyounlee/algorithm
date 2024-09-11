c6a4e874f12a4110d7d1d06338d605657cade704
Miyeon Lee <leeyun92@naver.com>
2024-09-11 14:14:34 +0900

    [Silver V] Title: 크로아티아 알파벳, Time: 100 ms, Memory: 14264 KB -BaekjoonHub

---
+++ 백준/Silver/2941. 크로아티아 알파벳/크로아티아 알파벳.java
@@ -0,0 +1,21 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String str = br.readLine();
+
+        String[] arr = {"c=", "c-", "dz=", "d-", "lj", "nj", "s=", "z="};
+
+        for (int i = 0; i < arr.length; i++) {
+            if (str.contains(arr[i])) {
+                str = str.replaceAll(arr[i], "a");
+            }
+        }
+
+        System.out.println(str.length());
+    }
+}
